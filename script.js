23c78461577cfe6a06d1ca4ea87ff428930593d3
sam quave <samquave@sams-MacBook-Air.local>
2017-09-10 17:45:33 -0400

    shows first string; gives feedback

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
 $(document).ready(function() {
     $("#keyboard-upper-container").hide();
@@ -20,7 +20,9 @@ $(document).ready(function() {
         var keypressed = event.keyCode;
         if(event.shiftKey===true) {
         $("#" + keypressed).css("background-color", "yellow");
-        } else{
+        } else  if(keypressed===32) {
+            $("#32").css("background-color", "yellow");
+        }else {
             $("#" + (keypressed + 32)).css("background-color", "yellow");
         }
     })
@@ -28,11 +30,27 @@ $(document).ready(function() {
         var keypressed = event.keyCode;
         if(event.shiftKey===true) {
         $("#" + keypressed).css("background-color", "WhiteSmoke");
-        } else {
+        } else if(keypressed===32) {
+            $("#32").css("background-color", "WhiteSmoke");
+        }else {
             $("#" + (keypressed + 32)).css("background-color", "WhiteSmoke");
         }
         
     })
+    var i = 0;
+    $("#sentence").append(sentences[i]);
+    
+    $("body").keypress(function(event){
+        var keypressed = event.keyCode;
+        var targetchar = sentences[i].charCodeAt(0);
+        
+        if(keypressed===targetchar) {
+            $("#feedback").append('<span class="glyphicon glyphicon-ok"></span>');
+        } else{
+            $("#feedback").append('<span class="glyphicon glyphicon-remove"></span>');
+        }
+    })
+
 
 
 
