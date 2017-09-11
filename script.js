7f82bc5afec2166f5b69f1b14904053e38102473
sam quave <samquave@sams-MacBook-Air.local>
2017-09-10 21:08:32 -0400

    target indicator fixed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
+
 $(document).ready(function() {
     $("#keyboard-upper-container").hide();
     $("body").on("keydown", function(event) {
@@ -37,25 +38,58 @@ $(document).ready(function() {
         }
         
     })
-    var i = 0;
-    $("#sentence").append(sentences[i]);
+     var i = 0;
+     var charposition = 0;
     
+    $("#sentence").append(sentences[i]);
+   
     $("body").keypress(function(event){
         var keypressed = event.keyCode;
-        var targetchar = sentences[i].charCodeAt(0);
+        var targetchar = sentences[i].charCodeAt(charposition);
         
         if(keypressed===targetchar) {
             $("#feedback").append('<span class="glyphicon glyphicon-ok"></span>');
-        } else{
+            charposition++;   
+            $('#yellow-block').animate({left: '+=.73em'}, 50);
+                
+          
+
+        } else {
+        
+            
             $("#feedback").append('<span class="glyphicon glyphicon-remove"></span>');
         }
     })
 
+    $('body').keypress(function(event) {
+        var keypressed = event.keyCode;
+        var targetchar = sentences[i].charCodeAt(charposition);
+        var id = document.getElementById("sentence");
+        if(charposition>=sentences[i].length) {
+            i++;
+            while(id.firstChild) {
+                id.removeChild(id.firstChild);
+            }
+              
+              $('#sentence').append(sentences[i]);
+              charposition=0;
 
-
-
-    
+        }
     })
+    $("body").keypress(function(event) {
+        var keypressed = event.keyCode;
+        var targetchar = sentences[i].charCodeAt(charposition);
+        if(charposition===0)
+            $('#yellow-block').animate({
+                top: '1px',
+                left: '15px',
+            }, 0);
+ 
+    })
+    
+   
+
+})
     
     
     
